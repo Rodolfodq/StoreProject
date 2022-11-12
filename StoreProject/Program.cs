19abff04a9bc38bd0aedeecbde9810d76acc3296
Rodolfo Queiroz <rodolfo.dqueiroz@gmail.com>
2022-11-11 23:35:03 -0300

    Enable CORS

--- StoreProject/Program.cs
+++ StoreProject/Program.cs
@@ -20,6 +20,10 @@ namespace StoreProject
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }));
 
             var app = builder.Build();
 
@@ -29,6 +33,7 @@ namespace StoreProject
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseCors("corsapp");
 
             app.UseHttpsRedirection();
 
